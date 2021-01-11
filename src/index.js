e8b6e640985d0b74227a30e2afecdfc19d6346e0
Alia Peterson <alia@pnwebs.com>
2021-01-11 13:53:20 -0700

    Rename and update scss files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
-import './css/styles.scss';
+import './css/index.scss'
 import './scripts.js'
 import './domUpdates.js'
+import './fetchApi.js'
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/apple-logo-outline.png'
 import './images/apple-logo.png'
 import './images/chicken-parm.jpg'
@@ -16,5 +11,3 @@ import './images/green-apples.jpg'
 import './images/pancakes.jpg'
 import './images/search.png'
 import './images/seasoning.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
