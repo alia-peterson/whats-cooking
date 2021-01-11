9f190947c7f75fa2eb7a950aee4599bd02cde3bd
Alia Peterson <alia@pnwebs.com>
2021-01-10 21:11:40 -0700

    Move post user information functionality

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -13,6 +13,19 @@ const fetchApi = {
   getIngredientsData() {
     return fetch('http://localhost:3001/api/v1/ingredients')
       .then(response => response.json())
+  },
+
+  postUserInformation(updatedPantryItem) {
+    const postFormat = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(updatedPantryItem)
+    }
+
+    return fetch('http://localhost:3001/api/v1/users', postFormat)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+      // .catch(error => window.alert(`You do not have enough ${item.json()} for this recipe.`))
   }
 
 }
