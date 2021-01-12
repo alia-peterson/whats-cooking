1d288f9273004c467b9d49100d17485a9b9f0cf1
tashiad <tashiadavis10@gmail.com>
2021-01-12 15:58:35 -0700

    Remove extra h1s

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -19,7 +19,7 @@ let domUpdates = {
   addCardToDom(recipeInfo, shortRecipeName) {
     const newRecipeCard = cardTemplate.content.cloneNode(true)
     newRecipeCard.querySelector('article.recipe--card').id = recipeInfo.id
-    newRecipeCard.querySelector('h3.recipe--title').innerText = shortRecipeName
+    newRecipeCard.querySelector('h4.recipe--title').innerText = shortRecipeName
     newRecipeCard.querySelector('img.recipe--photo').src = recipeInfo.image
     newRecipeCard.querySelector('img.recipe--photo').alt = recipeInfo.name
     newRecipeCard.querySelector('img.recipe--photo').title = `${recipeInfo.name} recipe`
